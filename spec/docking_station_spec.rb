b9de6b009db48eaf77b6dc6a9fea6898b0d311b7
Eilw <eirikwiig@gmail.com>
2016-01-21 12:02:03 +0000

    ex 19 added doubles to DS class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,8 +5,8 @@ describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
   it 'check if bike is working' do
-    bike = Bike.new
-    expect(bike.working?).to eq true
+    #bike = Bike.new
+    expect(double(:bike).working?).to eq true
   end
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
@@ -14,19 +14,19 @@ describe DockingStation do
   it { is_expected.to respond_to(:bikes) }
 
   it 'dock bike' do
-    bike = Bike.new
-    expect(subject.dock_bike(bike)).to include bike
+    #bike = Bike.new
+    expect(subject.dock_bike(double(:bike))).to include double(:bike)
   end
 
   it 'check the same bike exists' do
-    bike = Bike.new
-    subject.dock_bike(bike)
-    expect(subject.bikes).to include bike
+    #bike = Bike.new
+    subject.dock_bike(double(:bike))
+    expect(subject.bikes).to include double(:bike)
   end
 
   it 'release a bike when it is available' do
-    bike = Bike.new
-    subject.dock_bike(bike)
+    #bike = Bike.new
+    subject.dock_bike(double(:bike))
     expect(subject.release_bike).to eq bike
   end
 
@@ -35,31 +35,8 @@ describe DockingStation do
   end
 
   it 'raise an error when station is full' do
-    DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(Bike.new) }
-    expect { subject.dock_bike(Bike.new) }.to raise_error("Station is full!")
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(double(:bike)) }
+    expect { subject.dock_bike(double(:bike)) }.to raise_error("Station is full!")
   end
 
-  it 'able to enter station capacity' do
-    station = DockingStation.new(50)
-    expect(station.capacity).to eq(50)
-  end
-
-  it 'capacity defaults to 20 if not added' do
-    #station = DockingStation.new
-    expect(subject.capacity).to eq(20)
-  end
-
-  it 'station has variable capacity' do
-    station = DockingStation.new(50)
-    50.times { station.dock_bike(Bike.new) }
-    expect { station.dock_bike(Bike.new) }.to raise_error("Station is full!")
-  end
-
-  it 'station does not release broken bikes' do
-    station = DockingStation.new
-    bike = Bike.new
-    bike.bike_broken
-    station.dock_bike(bike)
-    expect { station.release_bike }.to raise_error("Bike is broken")
-  end
 end
