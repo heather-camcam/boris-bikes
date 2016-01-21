4a83c620b4dd343f73e39308b6c9ef9c47283dec
Heather <campbell.hf@gmail.com>
2016-01-21 12:38:04 +0000

    ch20 finished - mocked behaviour using doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,14 @@ require 'docking_station'
 
 describe DockingStation do
 
+  let(:bike) {double(:bike, :working? => true,
+    :broken? => false)}
+
   it { is_expected.to respond_to(:release_bike) }
 
   it 'check if bike is working' do
-    #bike = Bike.new
-    expect(double(:bike).working?).to eq true
+    #allow(bike).to receive(:working?).and_return(true)
+    expect(bike.working?).to eq true
   end
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
@@ -14,19 +17,17 @@ describe DockingStation do
   it { is_expected.to respond_to(:bikes) }
 
   it 'dock bike' do
-    #bike = Bike.new
-    expect(subject.dock_bike(double(:bike))).to include double(:bike)
+    expect(subject.dock_bike(bike)).to include bike
   end
 
   it 'check the same bike exists' do
-    #bike = Bike.new
-    subject.dock_bike(double(:bike))
-    expect(subject.bikes).to include double(:bike)
+    subject.dock_bike(bike)
+    expect(subject.bikes).to include bike
   end
 
   it 'release a bike when it is available' do
-    #bike = Bike.new
-    subject.dock_bike(double(:bike))
+    #allow(bike).to receive(:broken?).and_return(false)
+    subject.dock_bike(bike)
     expect(subject.release_bike).to eq bike
   end
 
@@ -35,8 +36,8 @@ describe DockingStation do
   end
 
   it 'raise an error when station is full' do
-    DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(double(:bike)) }
-    expect { subject.dock_bike(double(:bike)) }.to raise_error("Station is full!")
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(bike) }
+    expect { subject.dock_bike(bike) }.to raise_error("Station is full!")
   end
 
 end
