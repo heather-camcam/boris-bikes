67641a6101079809f902bc50524026cf66b44e98
Eilw <eirikwiig@gmail.com>
2016-01-21 11:06:52 +0000

    1st user story in exercise 18

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,10 +1,16 @@
 
 
 class Bike
-
+  attr_reader :bike_broken
   def working?
     true
   end
 
+  def bike_broken
+    bike_broken = true
+  end
 
+  def broken?
+    true if bike_broken
+  end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,6 +4,13 @@ describe Bike do
 
   it {is_expected.to respond_to('working?')}
 
+  it {is_expected.to respond_to('bike_broken')}
+
+  it 'if reported broken, then bike does not work' do
+    subject.bike_broken
+    expect(subject).to be_broken
+  end
+
 # is expecting subject.working? to retrun true
   #it do
     #expect (subject.working?).to eq('true')
